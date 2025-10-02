88744e6412780997eef2c558c25a24a894ec867d
tkototyy <tkototyy@outlook.com>
2025-10-02 09:22:30 +0000

    Update ena.py

--- ena.py
+++ ena.py
@@ -1,18 +1,8 @@
-from seleniumbase import BaseCase
+from seleniumbase import SB
 import time
-
-class IPLeakScreenshot(BaseCase):
-    def test_ipleak_screenshot(self):
-        # Navigate to ipleak.net
-        self.open("https://ipleak.net")
-
-        # Wait 15 seconds to allow all checks to run
-        time.sleep(15)
-
-        # Save screenshot
-        self.save_screenshot("ipleak_screenshot.png")
-
-# To run directly:  python ipleak_screenshot.py
-if __name__ == "__main__":
-    from seleniumbase import run_command
-    run_command("ipleak_screenshot.py IPLeakScreenshot.test_ipleak_screenshot")
+import requests
+import sys
+import requests
+import os
+import random
+import subprocess
